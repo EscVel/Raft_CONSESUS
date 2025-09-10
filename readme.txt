f7a8229140b07a6c73003384973f9b60417bf96c
EscVel <suhithegde@gmail.com>
2025-09-10 19:10:08 +0530

    leader election working

--- readme.txt
+++ readme.txt
@@ -1,1 +1,11 @@
 first file
+cmd /c 'curl.exe -X POST -H "Content-Type: application/json" http://127.0.0.1:8001/join -d "{\"id\": \"node3\", \"addr\": \"127.0.0.1:7003\"}"'
+./Raft_CONSENSUS -id node1 -raft-addr 127.0.0.1:7001 -http-addr 127.0.0.1:8001 -data-dir data -bootstrap
+
+first run go build
+./Raft_CONSENSUS -id node1 -raft-addr 127.0.0.1:7001 -http-addr 127.0.0.1:8001 -data-dir data -bootstrap
+./Raft_CONSENSUS -id node2 -raft-addr 127.0.0.1:7002 -http-addr 127.0.0.1:8002 -data-dir data
+./Raft_CONSENSUS -id node3 -raft-addr 127.0.0.1:7003 -http-addr 127.0.0.1:8003 -data-dir data
+
+cmd /c 'curl.exe -X POST -H "Content-Type: application/json" http://127.0.0.1:8001/join -d "{\"id\": \"node3\", \"addr\": \"127.0.0.1:7003\"}"'
+cmd /c 'curl.exe -X POST -H "Content-Type: application/json" http://127.0.0.1:8001/join -d "{\"id\": \"node2\", \"addr\": \"127.0.0.1:7002\"}"'
--- server.go
+++ server.go
@@ -1,3 +1,4 @@
+// server.go (Updated with /status endpoint)
 package main
 
 import (
@@ -24,7 +25,7 @@ func NewServer(addr string, store *Store) *Server {
 func (s *Server) Start() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/join", s.handleJoin)
-	// We will add more handlers here later for /set, /get, etc.
+	mux.HandleFunc("/status", s.handleStatus) // <-- ADD THIS LINE
 
 	log.Printf("HTTP server listening on %s\n", s.httpAddr)
 	return http.ListenAndServe(s.httpAddr, mux)
@@ -32,6 +33,7 @@ func (s *Server) Start() error {
 
 // handleJoin is the HTTP handler for joining a node to the cluster.
 func (s *Server) handleJoin(w http.ResponseWriter, r *http.Request) {
+	// ... (this function remains unchanged)
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -54,4 +56,16 @@ func (s *Server) handleJoin(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+// handleStatus is the HTTP handler for checking the node's Raft status.
+func (s *Server) handleStatus(w http.ResponseWriter, r *http.Request) { // <-- ADD THIS ENTIRE FUNCTION
+	w.Header().Set("Content-Type", "application/json")
+	status := map[string]string{
+		"status": s.store.raft.State().String(),
+	}
+	if err := json.NewEncoder(w).Encode(status); err != nil {
+		log.Printf("Failed to encode status: %s", err)
+		http.Error(w, "Failed to encode status", http.StatusInternalServerError)
+	}
 }
